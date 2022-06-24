a9ab23150aa92a47d5a778772b7a825f4eaf20aa
Evgeny Zhukovets <sokzhukkk@mail.ru>
2022-06-24 19:50:45 +0300

    initional commit

---
+++ stack.py
@@ -0,0 +1,17 @@
+from sqlite3 import paramstyle
+import requests
+
+from datetime import date, timedelta
+from pprint import pprint
+
+def stack_py():
+    teg = 'python'
+    today = date.today()
+    yesterday = today - timedelta(days=1)
+    host = 'https://api.stackexchange.com/'
+    params = {'fromdate': yesterday, 'todate' : today, 'tagged':teg, 'site':'stackoverflow'}
+    resp = requests.get(f'{host}/2.3/questions', params = params)
+    pprint(resp.json())
+
+
+stack_py()
